7fe5781d1a9837381ebccda4841e7747dc871149
Antoine Choppin <antoine@japonophile.com>
2020-08-01 14:50:37 +0900

    Allow to toggle cells and update a single step

--- src/game_of_life/core.cljs
+++ src/game_of_life/core.cljs
@@ -22,51 +22,6 @@
 
 (println "The Game of Life")
 
-(defn render-world
-  [world]
-  (let [canvas (.getElementById js/document "world")
-        ctx (.getContext canvas "2d")]
-    (doall (for [r (range height)
-                 c (range width)]
-             (do
-               (if (= 1 (get-in world [r c]))
-                 (set! (.-fillStyle ctx) "rgb(0,0,0)")
-                 (set! (.-fillStyle ctx) "rgb(255,255,255)"))
-               (.beginPath ctx)
-               (.rect ctx (* d c) (* d r) d d)
-               (.fill ctx))))))
-
-(defn world-component
-  []
-  (r/create-class
-    {
-     :component-did-update
-     (fn [_]
-       (render-world (:world @app-state)))
-     :component-did-mount
-     (fn [_]
-       (render-world (:world @app-state)))
-     :reagent-render
-     (fn []
-       (:world @app-state)
-       [:div {:style {:width "100%"}}
-        [:canvas {:id "world" :width (str (* d width)) :height (str (* d height))
-                  :style {:margin "auto" :border "1px solid black"}}]])}))
-
-(defn app-component
-  []
-  [:div
-   [:h1 "The Game of Life"]
-   [world-component]
-   [:label.switch
-    [:input {:type "checkbox" :checked (:update? @app-state)
-             :on-change (fn [e] (swap! app-state assoc :update? (-> e .-target .-checked)))}]
-    [:span.slider.round]]
-   [:p (str "Step: " (:steps @app-state))]])
-
-(rd/render [app-component]
-           (. js/document (getElementById "app")))
-
 (defn count-neighbors
   [world r c]
   (let [r1 (max 0 (dec r))
@@ -87,12 +42,80 @@
                     (if (and (< 1 n) (< n 4)) 1 0)   ; 1
                     (if (= 3 n) 1 0))))))))          ; 0
 
+(defn single-step-update
+  [state]
+  (-> state
+    (assoc :world (update-world (:world state)))
+    (assoc :steps (inc (:steps state)))))
+
 (defn update-state
   [state]
   (if (:update? state)
-    (-> state
-      (assoc :world (update-world (:world state)))
-      (assoc :steps (inc (:steps state))))
+    (single-step-update state)
     state))
+
+(defn render-world
+  [world]
+  (let [canvas (.getElementById js/document "world")
+        ctx (.getContext canvas "2d")]
+    (doall (for [r (range height)
+                 c (range width)]
+             (do
+               (if (= 1 (get-in world [r c]))
+                 (set! (.-fillStyle ctx) "rgb(0,0,0)")
+                 (set! (.-fillStyle ctx) "rgb(255,255,255)"))
+               (.beginPath ctx)
+               (.rect ctx (* d c) (* d r) d d)
+               (.fill ctx))))))
+
+(defn toggle-cell
+  [r c]
+  (swap! app-state update-in [:world r c] #(- 1 %)))
+
+(defn handle-click-event
+  [e]
+  (let [rect (-> e .-target .getBoundingClientRect)
+        t (-> rect .-top)
+        l (-> rect .-left)
+        r (/ (- (-> e .-clientY) t) d)
+        c (/ (- (-> e .-clientX) l) d)
+        _ (prn rect t l r c)]
+    (toggle-cell r c)))
+
+(defn world-component
+  []
+  (r/create-class
+    {
+     :component-did-update
+     (fn [_]
+       (render-world (:world @app-state)))
+     :component-did-mount
+     (fn [_]
+       (render-world (:world @app-state)))
+     :reagent-render
+     (fn []
+       (:world @app-state)
+       [:div {:style {:width "100%"}}
+        [:canvas {:id "world" :width (str (* d width)) :height (str (* d height))
+                  :style {:margin "auto" :border "1px solid black"}
+                  :on-click handle-click-event}]])}))
+
+(defn app-component
+  []
+  [:div
+   [:h1 "The Game of Life"]
+   [world-component]
+   [:div
+    [:span "Auto-update: "]
+    [:label.switch
+     [:input {:type "checkbox" :checked (:update? @app-state)
+              :on-change (fn [e] (swap! app-state assoc :update? (-> e .-target .-checked)))}]
+     [:span.slider.round]]
+    [:span {:style {:display "inline-block" :width "40px"}} " "]
+    [:button.btn {:on-click #(swap! app-state single-step-update)} [:i.fa.fa-step-forward]]]
+   [:p (str "Step: " (:steps @app-state))]])
+
+(rd/render [app-component]
+           (. js/document (getElementById "app")))
 
 (js/setInterval #(swap! app-state update-state) step-time-ms)
